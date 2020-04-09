ea1226543dfedb735d2f7644190b6251d9dbe7d4
ssriram <pingssriram@gmail.com>
2020-04-09 23:46:29 +0530

    modified alexa

--- alexa.rb
+++ alexa.rb
@@ -1,15 +1,38 @@
 
+require 'sequel'
 require 'open-uri'
 require 'nokogiri'
 require 'csv'
 
-CSV.open("alexa-ind-500.csv", "w") do |f|
+FILENAME = "alexa-ind-500.db"
 
-  (0..19).each do |n|
-    url = "http://www.alexa.com/topsites/countries;#{n}/IN"
-    doc = Nokogiri::HTML(open(url))
-    doc.css('li.site-listing h2 a').each do |link|
-      f << [link.text]
+#SELECTOR = "li.site-listing h2 a"
+SELECTOR = ".desc-paragraph a"
+
+
+def pull_data
+
+  Sequel.sqlite(FILENAME) do |db|
+    id = 1
+    day = Time.now
+
+    (0..20).each do |n|
+      url = "http://www.alexa.com/topsites/countries;#{n}/IN"
+      doc = Nokogiri::HTML(open(url))
+      db.transaction do  
+        doc.css(SELECTOR).each do |link|
+          puts "#{link.text.downcase}, #{id}, #{day}"
+          db[:links].insert(:id => id, :link => link.text.downcase, :entry_dt => day)
+          id += 1
+        end
+      end
     end
+
   end
+
 end
+
+
+pull_data
+
+
